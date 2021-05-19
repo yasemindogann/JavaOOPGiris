290e162f0976caae2d6d882353438262c651d7a2
Yasemin DOĞAN <40598440+yasemindogann@users.noreply.github.com>
2021-05-19 18:24:10 +0300

    Update CourseManager.java

--- src/ikinciDersOdev1/CourseManager.java
+++ src/ikinciDersOdev1/CourseManager.java
@@ -2,14 +2,17 @@ package ikinciDersOdev1;
 
 public class CourseManager {
 	
+	//Metot
 	public void addCourse(Course course) {
 		System.out.println("Kurs eklendi: " + course.courseName);
 	}
 	
+	//Metot
 	public void deleteCourse(Course course) {
 		System.out.println("Kurs silindi: " + course.courseName);
 	}
 	
+	//Metot
 	public void contentDelete(Course course) {
 		System.out.println("Kurs içeriği silindi: " + course.courseDetail);
 	}
